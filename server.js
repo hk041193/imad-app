0ddccef706bc4ff519f3f1bc4c1e3d1e9291dc5d
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-24 20:05:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'hk041193',
+    database: 'hk041193',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.emv.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
