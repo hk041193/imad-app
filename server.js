f8fce26cb5bebc5ebabbb9e4b6c3bb68767553d9
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-25 15:13:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/article/:articleName', function (req, res) {
                 res.status(404).send('Article not found.');
             } else {
                 var articleData = result.rows[0];
-                res.send(createTemplate(articlesData));
+                res.send(createTemplate(articleData));
             }
         }
     });
