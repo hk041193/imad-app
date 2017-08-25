26dac956ca39d0b48bf71ca6b83ee29a101cb60e
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-25 14:17:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'hk04193',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.emv.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
