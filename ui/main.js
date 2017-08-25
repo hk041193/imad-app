f913ee9af7d65ac433707115cbefb8f9e99b6052
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-25 13:51:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,3 +55,34 @@ submit.onclick = function() {
     request.send(null);
 };
 
+//Submit comment
+var submit = document.getElementById('submit_btn4');
+submit.onclick = function() {
+    
+    //Create request to object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+                //capture a list of names and render it
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var list='';
+                for(var i=0; i<comments.length; i++){
+                    list += '<li>' + comments[i] + '</li>';
+                }
+                var ul = document.getElementById('commentlist');
+                ul.innerHTML = list;
+           }
+       } 
+    };
+    
+    //make the request
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+    request.open('GET', 'http://hk041193.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+    request.send(null);
+};
