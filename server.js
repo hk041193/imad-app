adcbed50bb09c43efba904aa3ed7d19fb94cfb81
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-25 14:14:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'hk041193',
+    database: 'hk04193',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.emv.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -83,7 +92,20 @@ function createTemplate (data) {
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    //make a select request
+    //return the response with the result
+    pool.query('SELECT * FROM test', function(err, result) {
+        if(err) {
+            res.return(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter = 0;
@@ -106,15 +128,15 @@ app.get('/:articleName', function (req, res) {
 });  
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
