47c541adac75a83b6d0a8aef753a454ecfcf3d5a
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-25 15:11:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.get('/article/:articleName', function (req, res) {
     var articleName = req.params.articleName;
     
     var articleData =
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result) {
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
