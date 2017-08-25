b64489b146eb15301d7e1421a7836d7c002c65a6
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-25 13:36:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-four', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-four.html'));
+});
+
 var counter = 0;
 app.get('/counter', function (req, res){
     counter = counter +1;
