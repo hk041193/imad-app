a397acc085838a99529f8329936adbc88ab66db0
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-25 14:25:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 
 var config = {
     user: 'hk041193',
-    database: 'hk04193',
+    database: 'hk041193',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
