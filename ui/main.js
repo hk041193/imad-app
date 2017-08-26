46b96c0579ca4a738ec77eb9c20dc321311d9e0c
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-26 20:51:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,38 @@ button.onclick = function () {
     request.send(null);
 };
 
+//Submit name
+var submit = document.getElementById('submit_btn1');
+submit.onclick = function() {
+    
+    //Create request to object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+               //capture a list of names and render it
+               var names = request.responseText;
+               names = JSON.parse(names);
+               var list = '';
+               for (var i=0; i< names.length; i++) {
+                   list == '<li>' + names[i] + '</li>';
+               }
+               var ul = document.getElementById('namelist');
+               ul.innerHTML.list;
+           }
+       } 
+    };
+    
+    //make the request
+    var nameInput = document.getElementById('nameInput');
+    var name = nameInput.value;
+    request.open('GET', 'http://hk041193.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
+
 //Submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
