d7b40c561a9839730ecbb48cb9ad804ac573772c
hk041193 <30732228+hk041193@users.noreply.github.com>
2017-08-26 21:16:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/check-login', function(req, res) {
     }
 });
 
-app.get('/logged-out', function(req, res) {
+app.get('/logout', function(req, res) {
     delete req.session.auth;
     res.send('You are logged out');
 });
